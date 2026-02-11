f90d9ff891118a38513888f8d1668cc245f3b451
Amer Mohamed <131729780+AmerrMohamedd@users.noreply.github.com>
2026-02-11 23:15:47 +0200

    some changes

--- incidents/urls.py
+++ incidents/urls.py
@@ -9,13 +9,12 @@ from .views import (
 )
 
 urlpatterns = [
-    path('incident-statuses/', IncidentStatusListAPIView.as_view(), name='incident-statuses'),
-    path('incidents/', IncidentListAPIView.as_view(), name='incident-list'),
-    path('incidents/<int:pk>/', IncidentDetailAPIView.as_view(), name='incident-detail'),
-    path('incidents/create/', IncidentCreateAPIView.as_view(), name='incident-create'),
-    path('incidents/my/', MyIncidentsAPIView.as_view(), name='my-incidents'),
-    path('incidents/<int:pk>/images/', IncidentImageUploadAPIView.as_view(), name='incident-image-upload'),
-
+    path('statuses/', IncidentStatusListAPIView.as_view(), name='incident-statuses'),
+    path('', IncidentListAPIView.as_view(), name='incident-list'),
+    path('<int:pk>/', IncidentDetailAPIView.as_view(), name='incident-detail'),
+    path('create/', IncidentCreateAPIView.as_view(), name='incident-create'),
+    path('my/', MyIncidentsAPIView.as_view(), name='my-incidents'),
+    path('<int:pk>/images/', IncidentImageUploadAPIView.as_view(), name='incident-image-upload'),
 
 
 ]
